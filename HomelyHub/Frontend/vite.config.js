15fef92329822f85b72b2d21bb9532b2bcef22de
kollaaditya <kollaaditya968@gmail.com>
2026-01-24 22:09:19 +0530

    Update vite.config.js

--- HomelyHub/Frontend/vite.config.js
+++ HomelyHub/Frontend/vite.config.js
@@ -7,11 +7,10 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-<<<<<<< HEAD
+
         target:'https://homely-r26m.onrender.com',
-=======
-        target: "https://homely-r26m.onrender.com",
->>>>>>> d457bca2008b960ea38ec51b1ea62d61ad0a6270
+
+        
         changeOrigin: true,
         secure: false,
       },
